fae5f54ed1111e0d2465f4f49a16b9df04ab5b0d
Medini1dath7046 <medinipd@gmail.com>
2018-06-28 11:38:36 +0530

    asr

---
+++ TextEditor.java
@@ -0,0 +1,29 @@
+package com.bridgelabz.annotationsAutoWired;
+
+import org.springframework.beans.factory.annotation.Autowired;
+/**
+ * Created By:Medini P.D
+ * Date:- 27/06/2018
+ * Purpose:
+ */
+
+public class TextEditor {
+   private SpellChecker spellChecker;
+   private String name;
+   
+   public void setSpellChecker( SpellChecker spellChecker ){
+      this.spellChecker = spellChecker;
+   }
+   public SpellChecker getSpellChecker() {
+      return spellChecker;
+   }
+   public void setName(String name) {
+      this.name = name;
+   }
+   public String getName() {
+      return name;
+   }
+   public void spellCheck() {
+      spellChecker.checkSpelling();
+   }
+}
